109098dbbaacc8714ebadc276dba93c863bdefe8
maybekate <schpakova.katja@gmail.com>
2025-12-08 14:13:18 +0300

    Delete cypress directory

--- cypress/cypress.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  e2e: {
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
-    },
-    baseUrl:"https://p-sergei-qa.github.io/"  //Выносим baseURL в конфиг, в js указывать '/'
-  },
-};
